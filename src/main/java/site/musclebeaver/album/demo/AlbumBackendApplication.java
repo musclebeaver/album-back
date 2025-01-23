a4ef785e7c0190417c562da29f814967eee71fa7
김민석 <95396532+biber5515@users.noreply.github.com>
2025-01-23 23:10:43 +0900

    Initial commit

---
+++ src/main/java/site/musclebeaver/album/demo/AlbumBackendApplication.java
@@ -0,0 +1,13 @@
+package site.musclebeaver.album.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/musclebeaver/album/demo/AlbumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.musclebeaver.album.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
